bc0add84da65d2068f08c1e365d97fdcb9653eae
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2022-01-28 12:38:21 +0330

    datasets prepare

---
+++ db.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#SBATCH --mail-user=Moslem.Yazdanpanah@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=baseline
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
+#SBATCH --time=0-10:00
+#SBATCH --account=rrg-ebrahimi
+
+nvidia-smi
+
+source ~/ENV_MD/bin/activate
+
+echo "------------------------------------< Data preparation>----------------------------------"
+echo "Copying the source code"
+date +"%T"
+cd $SLURM_TMPDIR
+
+cp -r ~/scratch/metadatasets/quickdraw/ .
+cp -r ~/scratch/metadatasets/mscoco/ .
+cd mscoco
+unzip train2017.zip
+unzip annotations_trainval2017.zip
+mv annotations/* .
+
+cd $SLURM_TMPDIR
+
+cp -r ~/scratch/meta-dataset .
+cd meta-dataset
+
+
+python -m meta_dataset.dataset_conversion.convert_datasets_to_records \
+  --dataset=quickdraw \
+  --quickdraw_data_root=../quickdraw \
+  --splits_root=../SPLITS \
+  --records_root=../records2
+
+python -m meta_dataset.dataset_conversion.convert_datasets_to_records \
+  --dataset=mscoco \
+  --mscoco_data_root=../mscoco \
+  --splits_root=../SPLITS \
+  --records_root=../records2
+
+echo "-----------------------------------<End of run the program>---------------------------------"
+date +"%T"
+echo "--------------------------------------<backup the result>-----------------------------------"
+date +"%T"
+cd $SLURM_TMPDIR
+cp -r $SLURM_TMPDIR/records2/ ~/scratch/metadatasets/
+
