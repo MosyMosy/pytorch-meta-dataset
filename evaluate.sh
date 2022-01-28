1ef2dd424b5945f004f7b4ee5f283626a8e48a4a
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2022-01-28 21:51:22 +0330

    evaluate

---
+++ evaluate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --mail-user=Moslem.Yazdanpanah@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=evaluate
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
+#SBATCH --time=1-00:00
+#SBATCH --account=rrg-ebrahimi
+
+nvidia-smi
+
+source ~/ENV_MD/bin/activate
+
+echo "------------------------------------< Data preparation>----------------------------------"
+echo "Copying the source code"
+date +"%T"
+cd $SLURM_TMPDIR
+
+cp -r ~/scratch/metadatasets .
+cp -r ~/scratch/pytorch-meta-dataset .
+cd pytorch-meta-dataset
+
+bash scripts/test_all.sh BN Resnet18
+
+echo "-----------------------------------<End of run the program>---------------------------------"
+date +"%T"
+echo "--------------------------------------<backup the result>-----------------------------------"
+date +"%T"
+cd $SLURM_TMPDIR
+cp -r $SLURM_TMPDIR/pytorch-meta-dataset/results/ ~/scratch/pytorch-meta-dataset/
+
